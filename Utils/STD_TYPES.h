c3ae2ea86b15d07aa7d4a5badeb01b3805601c1d
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-09 00:23:42 +0200

    Add STD_TYPES.h for standard type definitions

---
+++ Utils/STD_TYPES.h
@@ -0,0 +1,15 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char       uint8_t;
+typedef signed char         sint8_t;
+typedef unsigned short      uint16_t;
+typedef signed short        sint16_t;
+typedef unsigned long       uint32_t;
+typedef signed long         sint32_t;
+typedef float               float32_t;
+typedef double              float64_t;
+
+#define NULL_PTR    ((void*)0)
+
+#endif
