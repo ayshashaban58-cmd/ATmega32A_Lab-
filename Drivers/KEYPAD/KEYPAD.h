2d8ab014e8ebc2090c962d58511616e5b59aae8d
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-09 01:20:39 +0200

    Add KEYPAD header file with initialization and key retrieval

---
+++ Drivers/KEYPAD/KEYPAD.h
@@ -0,0 +1,17 @@
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+#include "../../Utils/STD_TYPES.h"
+
+#define KEYPAD_PORT     DIO_PORTD
+
+// لو عندك كيباد 4x4 غيري الرموز هنا
+#define KPD_ARR         {{'1','2','3','A'},\
+                         {'4','5','6','B'},\
+                         {'7','8','9','C'},\
+                         {'*','0','#','D'}}
+
+void KEYPAD_voidInit(void);
+uint8_t KEYPAD_u8GetPressedKey(void);
+
+#endif
